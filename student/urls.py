56781e73fd9471d0f2442eb802f076e3cb3b7fd8
haeydee <149032989+haeydee@users.noreply.github.com>
2025-06-05 22:38:49 +0800

    Delete student directory

--- student/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dashboard/', views.dashboard, name ='student_dashboard'),
-    path('milestone/', views.milestone_view, name='milestone'),
-    path('milestone/delete/<int:pk>/', views.delete_milestone, name='delete_milestone'),
-]
